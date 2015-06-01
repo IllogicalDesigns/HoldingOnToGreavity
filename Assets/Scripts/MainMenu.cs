3f230359e4d6baf582b4c83a11b7acd568e8e8c3
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-05-31 23:40:45 -0700

    Second Commit, back to big comp

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+	public GameObject main;
+	public GameObject host;
+	public GameObject join;
+	public Text gName;
+	public Text numberOfPlayers;
+	public string typeName = "UniqueGameName";
+	private string gameName = "RoomName";
+
+	// Use this for initialization
+	void Start () {
+		main.SetActive(true);
+		host.SetActive(false);
+		join.SetActive(false);
+	}
+	public void SwitchToHost () {
+		main.SetActive(false);
+		host.SetActive(true);
+		join.SetActive(false);
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+	}
+
+	
+	public void StartServer()
+	{
+		typeName = gName.ToString ();
+		Network.InitializeServer(4 , 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+		Debug.Log (typeName.ToString ());
+	}
+}
